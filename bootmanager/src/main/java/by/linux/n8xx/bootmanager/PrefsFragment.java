e077735a84e2ce8a75929743d1f13639d45a96f0
X-Stranger <xstranger@gmail.com>
2014-01-03 00:04:36 +0300

    Initial import

---
+++ bootmanager/src/main/java/by/linux/n8xx/bootmanager/PrefsFragment.java
@@ -0,0 +1,14 @@
+package by.linux.n8xx.bootmanager;
+
+import android.os.Bundle;
+import android.support.v4.preference.PreferenceFragment;
+
+public class PrefsFragment extends PreferenceFragment {
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        // Load the preferences from an XML resource
+        addPreferencesFromResource(R.xml.preferences);
+    }
+}
---
+++ bootmanager/src/main/java/by/linux/n8xx/bootmanager/items/Content.java
@@ -0,0 +1,57 @@
+package by.linux.n8xx.bootmanager.items;
+
+import android.content.res.Resources;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+import by.linux.n8xx.bootmanager.ItemListActivity;
+import by.linux.n8xx.bootmanager.R;
+
+/**
+ * Helper class for content for user interfaces.
+ * <p>
+ */
+public class Content {
+
+    /**
+     * An array of UI list items.
+     */
+    public static List<Item> ITEMS = new ArrayList<Item>();
+
+    /**
+     * A map of sample UI list items, by ID.
+     */
+    public static Map<String, Item> ITEM_MAP = new HashMap<String, Item>();
+
+    static {
+        addItem(new Item("1", ItemListActivity.getAppContext().getResources().getString(R.string.normal_boot)));
+        addItem(new Item("2", ItemListActivity.getAppContext().getResources().getString(R.string.recovery_boot)));
+        addItem(new Item("3", ItemListActivity.getAppContext().getResources().getString(R.string.settings)));
+    }
+
+    private static void addItem(Item item) {
+        ITEMS.add(item);
+        ITEM_MAP.put(item.id, item);
+    }
+
+    /**
+     * A dummy item representing a piece of content.
+     */
+    public static class Item {
+        public String id;
+        public String content;
+
+        public Item(String id, String content) {
+            this.id = id;
+            this.content = content;
+        }
+
+        @Override
+        public String toString() {
+            return content;
+        }
+    }
+}
